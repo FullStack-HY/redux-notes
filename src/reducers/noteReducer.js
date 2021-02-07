71cfd3f66a16008ceb617e372e9628ac6adc385b
Matti Luukkainen <mluukkai@iki.fi>
2021-02-07 23:44:59 +0200

    part6-3

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,17 +1,4 @@
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
-const noteReducer = (state = initialState, action) => {
+const noteReducer = (state = [], action) => {
   switch(action.type) {
     case 'NEW_NOTE':
       return [...state, action.data]
@@ -25,22 +12,17 @@ const noteReducer = (state = initialState, action) => {
       return state.map(note =>
         note.id !== id ? note : changedNote 
       )
+    case 'INIT_NOTES':
+      return action.data
     default:
       return state
   }
 }
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
-export const createNote = (content) => {
+export const createNote = (data) => {
   return {
     type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId()
-    }
+    data,
   }
 }
 
@@ -48,6 +30,13 @@ export const toggleImportanceOf = (id) => {
   return {
     type: 'TOGGLE_IMPORTANCE',
     data: { id }
+  }
+}
+
+export const initializeNotes = (notes) => {
+  return {
+    type: 'INIT_NOTES',
+    data: notes,
   }
 }
 
